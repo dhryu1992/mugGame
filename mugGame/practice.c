b8a2fc83f4d13ef22f228d96d74a4857529d17bd
dhryu1992 <sscross0727@gmail.com>
2020-07-31 11:15:27 +0900

    after moving

---
+++ mugGame/practice.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+
+
+	//char pp[6] = "ABCDE";
+	//printf("%s\n", pp);
+	//printf("%d\n", pp);
+	//printf("%s\n", pp + 3);
+	//printf("%d\n\n", pp + 3);
+
+	//char *pt;
+	//pt = "ABCDE";
+	//printf("%s\n\n", pt);
+
+	//pt = pp;
+
+	//printf("%s\n", pt);
+	//printf("%d\n", pt);
+	//printf("%s\n", pt + 3);
+	//printf("%d\n", pt + 3);
+
+	//return 0;
+
+};
