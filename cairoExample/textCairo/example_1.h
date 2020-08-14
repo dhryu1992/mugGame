b3bba6e878ef9ac2179828861c11800ad7ebff94
elcrity <elcrity@naver.com>
2020-08-14 13:35:19 +0900

    시연용

--- cairoExample/textCairo/example_1.h
+++ cairoExample/textCairo/example_1.h
@@ -1,17 +1,22 @@
-#ifndef CAIRO_T
+#ifndef EXAMPLE_1_H
 #include <string.h>
 #include <stdio.h>
 
 
 
-int i, a;
-char *text[10], *img[10]; 
-int k;
-int t;
+int i = 0;
+int a = 0;
+char *text[10] = {0,};
+char *img[10] = {0,};
+char *height[10] = {0, }; 
+int iNum = 0;
+int t = 0;
+int hNum = 0;
+int k = 0;
 
-char *naver[10] = {"abcd", "dfer", "src", "arin.png", "google", "smart", "src", "arin1.png"};
 
-char Array();
+char *naver[10] = { "google", "src", "arin1.png","height", "270", "src", "abc.png","height", "270" ,"chocolate", 0,};
+
 
 
 #endif
